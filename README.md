8c95a148f5ee7162f17850f644358743170e4787
Markuss Grinhāgens <markussgr@gmail.com>
2025-01-25 21:37:19 +0200

    Ready. Set. Go!

---
+++ README.md
@@ -0,0 +1,23 @@
+# American roulette picture bets
+This game tests your ability to associate common chip combinations (picture bets) with their payout value in the casino game American Roulette.
+
+The goal is to click the correct numerical payout value for each illustrated picture bet. 
+
+Incorrect answers will be coloured in red whereas correct answers will be coloured in green.
+
+When the correct answer is pressed, a button labelled "next" is revealed, granting the ability to move to the next picture bet.
+
+# Amerikāņu ruletes "picture bets"
+Šī spēle pārbauda Jūsu spēju saistīt bieži sastopamas žetonu kombinācijas (picture bets) ar to izmaksu vērtību kazino spēlē - Amerikāņu rulete.
+
+Mērķis ir klikšķināt pareizo izmaksas vērtību katram ilustrētajam "picture bet".
+
+Nepareizās atbildes tiks iekrāsotas sarkanā krāsā un pareizās atbildes tiks iekrāsotas zaļā krāsā.
+
+Kad nospiež pareizo atbildi, tiek atklāta poga ar uzrakstu "next", kas dod iespēju pāriet uz nākamo "picture bet".
+
+## future development plans
+- [ ] at end of game show average time to answer and amount of mistakes (combine into score value)
+- [ ] add splash screen with start button
+    - [ ] setting for amount of answer options
+- [ ] add all variations of identical value picture bets
---
+++ script.js
@@ -0,0 +1,98 @@
+var index = 0
+const picture_bets = [{ chips: [{ x: 0, y: 0 }, { x: 1, y: 3 }], answer: 40 },
+{ chips: [{ x: 0, y: 0 }, { x: 0, y: 1 }, { x: 0, y: 2 }], answer: 30 },
+{ chips: [{ x: 0, y: 2 }, { x: 1, y: 3 }, { x: 2, y: 4 }], answer: 51 },
+{ chips: [{ x: 0, y: 3 }, { x: 1, y: 3 }], answer: 52 },
+{ chips: [{ x: 0, y: 2 }, { x: 0, y: 3 }], answer: 25 },
+{ chips: [{ x: 0, y: 2 }, { x: 1, y: 2 }, { x: 2, y: 2 }], answer: 33 },
+{ chips: [{ x: 0, y: 2 }, { x: 1, y: 3 }, { x: 2, y: 2 }], answer: 51 },
+{ chips: [{ x: 0, y: 2 }, { x: 1, y: 2 }, { x: 1, y: 3 }], answer: 60 },
+{ chips: [{ x: 0, y: 3 }, { x: 1, y: 2 }, { x: 1, y: 3 }, { x: 1, y: 4 }, { x: 2, y: 3 }], answer: 103 },
+{ chips: [{ x: 0, y: 2 }, { x: 0, y: 4 }, { x: 1, y: 3 }, { x: 2, y: 2 }, { x: 2, y: 4 }], answer: 67 },
+{ chips: [{ x: 0, y: 2 }, { x: 0, y: 3 }, { x: 1, y: 2 }, { x: 1, y: 3 }, { x: 2, y: 2 }, { x: 2, y: 3 }], answer: 102 },
+{ chips: [{ x: 0, y: 0 }, { x: 0, y: 1 }, { x: 1, y: 0 }, { x: 1, y: 1 }, { x: 2, y: 0 }, { x: 2, y: 1 }], answer: 90 },
+{ chips: [{ x: 0, y: 2 }, { x: 0, y: 3 }, { x: 0, y: 4 }, { x: 1, y: 3 }, { x: 2, y: 2 }, { x: 2, y: 3 }, { x: 2, y: 4 }], answer: 101 },
+{ chips: [{ x: 0, y: 2 }, { x: 0, y: 3 }, { x: 0, y: 4 }, { x: 1, y: 2 }, { x: 1, y: 4 }, { x: 2, y: 2 }, { x: 2, y: 3 }, { x: 2, y: 4 }], answer: 100 },
+{ chips: [{ x: 0, y: 2 }, { x: 0, y: 3 }, { x: 0, y: 4 }, { x: 1, y: 2 }, { x: 1, y: 3 }, { x: 1, y: 4 }, { x: 2, y: 2 }, { x: 2, y: 3 }, { x: 2, y: 4 }], answer: 135 }]
+
+const order = shuffle(Array.from({ length: picture_bets.length }, (_, index) => index))
+
+function shuffle(array) {
+    // Fisher-Yates Shuffle Algorithm
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+function add_circle(parent, x, y, radius = 2) {
+    const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
+    circle.setAttribute("cx", x)
+    circle.setAttribute("cy", y)
+    circle.setAttribute("r", radius)
+    parent.appendChild(circle)
+}
+function populate_table(chips, x_offset = 12, y_offset = 0, x_step = 6, y_step = 5) {
+    // console.log(`adding picture bet chips ${JSON.stringify(chips)}`)
+    const table = document.getElementById('table')
+    chips.forEach((element, index, array) => {
+        add_circle(table, (element.x * x_step) + x_offset, (element.y * y_step) + y_offset)
+    });
+}
+function handle_answer(event) {
+    // console.log("handling button:", event)
+    if (this.className == "") {
+        if (parseInt(this.textContent) == picture_bets[order[index]].answer) {
+            this.classList.add('correct')
+            document.getElementById('next').removeAttribute('hidden')
+        } else {
+            this.classList.add('incorrect')
+        }
+    }
+}
+function remove_children(parent, type) {
+    const children = parent.querySelectorAll(type);
+    children.forEach(child => parent.removeChild(child));
+}
+function handle_next() {
+    document.getElementById('next').setAttribute('hidden', 'hidden')
+    const table = document.getElementById('table')
+    const buttons = document.getElementById('buttons')
+    remove_children(table, 'circle')
+    remove_children(buttons, 'button')
+    index++
+    if (index < picture_bets.length) {
+        populate_table(picture_bets[order[index]].chips)
+        populate_buttons(picture_bets[order[index]].answer)
+    } else {
+        // show results
+        h2 = document.createElement('h2')
+        h2.textContent = "thanks for playing, reload page to play again"
+        document.body.appendChild(h2)
+    }
+}
+function add_button(parent, text) {
+    const button = document.createElement('button')
+    button.textContent = text
+    button.addEventListener('click', handle_answer)
+    parent.appendChild(button)
+}
+function populate_buttons(answer) {
+    const buttons = document.getElementById('buttons')
+    const button_count = 3
+    n = order[index] % button_count
+    for (let i = 0; i < n; i++) {
+        add_button(buttons, answer - (n - i))
+    }
+    add_button(buttons, answer)
+
+    for (let i = 0; i < button_count - (n + 1); i++) {
+        add_button(buttons, answer + (i + 1))
+    }
+}
+
+addEventListener('load', (event) => {
+    populate_table(picture_bets[order[index]].chips)
+    populate_buttons(picture_bets[order[index]].answer)
+})
